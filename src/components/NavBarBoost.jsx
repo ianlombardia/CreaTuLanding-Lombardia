115aeb94ef99dd5532f011e3f4be1ef8d5bd6fdf
ian lombardia <ilombardia26@gmail.com>
2025-09-21 03:37:27 -0300

    actualizacion

--- src/components/NavBarBoost.jsx
+++ src/components/NavBarBoost.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
 import { NavLink } from 'react-router-dom';
-import "../css/navbarB.css";
+import '../css/navbarb.css';
 
 function NavBarBoost() {
   return (
